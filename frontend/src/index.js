377e280c455393ff2dc1186ee30c34be0824cf42
joshuahuggins-prog <joshua.huggins@googlemail.com>
2026-01-10 13:25:28 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
-
 import { initStorage } from "./utils/storage";
-
-initStorage();
 
 // PWA service worker
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
+// Initialize local storage
+initStorage();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
